71b92ffa8d3259774c8687ec4d72d5f4e8c1cff6
thuxuxs <xuxs15@mails.tsinghua.edu.cn>
2018-10-11 11:15:37 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-version = '0.0.1'
+version = '0.0.2'
 install_requires = ['numpy',
                     'matplotlib',
-                    'pandas']
+                    'pandas',
+                    'scipy']
